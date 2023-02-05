af338483d6397519ed9771b63d1af5097a49de8d
adopasado <metafamily86@gmil.com>
2023-02-05 12:22:50 -0300

    create services

--- src/product/interfaces/product.interface.ts
+++ src/product/interfaces/product.interface.ts
@@ -1,4 +1,6 @@
-export interface Product {
+import { Document } from 'mongoose';
+
+export interface Product extends Document {
   readonly name: string;
   readonly description: string;
   readonly price: number;
